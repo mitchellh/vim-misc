6b3cca55b3cfcc2b9bbd25b25b899392fc11ae5d
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-06-09 08:54:33 -0700

    gopls

--- lua/vim-misc.lua
+++ lua/vim-misc.lua
@@ -31,7 +31,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { "go" }
+local servers = { "gopls" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup { on_attach = on_attach }
 end
