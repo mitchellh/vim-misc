81684705cae024a9e1a7eab1dd4a5c4f33384f9d
shayne <79330+shayne@users.noreply.github.com>
2023-07-16 09:37:02 -0400

    Add rnix lsp

--- lua/vim-misc.lua
+++ lua/vim-misc.lua
@@ -46,7 +46,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { "gopls" }
+local servers = { "gopls", "rnix" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup { on_attach = on_attach }
 end
